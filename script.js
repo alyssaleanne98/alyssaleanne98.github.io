bbc1f897b8f4ca986470c422fc7131020de3ffab
Alyssa Cervantes (she/her) <112597248+alyssaleanne98@users.noreply.github.com>
2022-10-03 02:06:29 -0700

    update

---
+++ script.js
@@ -0,0 +1,15 @@
+// // mouse event 
+// const div = document.querySelector("first-header")
+// div.addEventListener("touch", e =>{
+//     console.log("You clicked the mouse");
+//     div.tagName = "hi";
+// })
+
+// function changeText(obj, text);
+// obj.innerText = text;
+
+
+// function mouseOver(){
+//     document.getElementById("optimist").textContent = "dog-lover";
+// }
+
