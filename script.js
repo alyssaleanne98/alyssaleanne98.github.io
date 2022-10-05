8e04debf768ef772fa374259c59450aaf7f53c45
Alyssa Cervantes (she/her) <112597248+alyssaleanne98@users.noreply.github.com>
2022-10-05 03:19:43 -0700

    update

--- script.js
+++ script.js
@@ -2,12 +2,23 @@
 // function mouseOver() {
 //     document.getElementByClass("first-header").style.color = "pink";
 //   }
-  
+
 // function mouseOut() {
 //      document.getElementByClass("first-header").style.color = "red";
 
 // resource: https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_onmouseover_html
 
 
-const h1 = document.getElementsByClassName("first-header") 
-console.log(h1)
+const h1 = document.getElementsByClassName("first-header")
+console.log(h1[0])
+
+h1[0].addEventListener("mouseover", (event) => {
+    if (h1[0].style.color === "orange") {
+        h1[0].style.color = "green"
+    }
+    else { h1[0].style.color = "orange"; }
+    console.log(h1[0])
+})
+
+
+
